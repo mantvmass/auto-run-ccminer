754b4bb704459ed191f9ea2007d6b19cb22c579c
Phumin Maliwan <32133224+mantvmass@users.noreply.github.com>
2022-02-28 21:40:48 +0700

    preset added

--- src_run_ccminer/edit.py
+++ src_run_ccminer/edit.py
@@ -1,45 +1,126 @@
 import os, json, time
+from config import banner
+from multiprocessing import cpu_count
 from progress.spinner import MoonSpinner
 
-# banner
-edit_banner = """
-███████╗██████╗ ██╗████████╗
-██╔════╝██╔══██╗██║╚══██╔══╝
-█████╗  ██║  ██║██║   ██║ 
-██╔══╝  ██║  ██║██║   ██║ 
-███████╗██████╔╝██║   ██║   
-╚══════╝╚═════╝ ╚═╝   ╚═╝"""
+cpu_thread = cpu_count()
 
-# banner function
-def banner(logo):
-    os.system("clear")
-    print(logo,"\nCreated by.mobile-mining")
-    print("---------------------------------------------------")
-    print("\033[96mสนับสนุนนักพัมนา\033[00m\n"
-        + " กสิกรไทย: 0608905863\n"
-        + "     LTC:  LTT2X57ervCMpfNva5uo1JBzT2UJVBiEZu\n"
-        + "    DOGE:  DFwUqKS3j3RMMkqQF8z6kbYZ652M8VCFVs")
-    print("---------------------------------------------------\n")
+def presetOne(): # zergpool
+    os.system("@cls||clear")
+    print("[--Time Zone--]\n"
+    ," [1] Default\n"
+    ," [2] North America\n"
+    ," [3] Europe\n"
+    ," [4] Asia")
+    try:
+        option = int(input(">>> "))
+        if option > 4 or option < 1:
+            raise Exception()
+        elif option == 1:
+            pool = "stratum+tcp://verushash.mine.zergpool.com:3300"
+        elif option == 2:
+            pool = "stratum+tcp://verushash.na.mine.zergpool.com:3300"
+        elif option == 3:
+            pool = "stratum+tcp://verushash.eu.mine.zergpool.com:3300"
+        elif option == 4:
+            pool = "stratum+tcp://verushash.asia.mine.zergpool.com:3300"
+    except:
+        os.system("@cls||clear")
+        print("เลือก 1 - 4 เท่านั้น!")
+        time.sleep(2)
+
+    coin = input("เหรียญที่จะรับ: ")
+    if coin == "":
+        coin = "VRSC"
+
+    wallet = input(f"ที่อยู่กระเป๋า {coin.upper()} : ")
+    if wallet == "":
+        os.system("@cls||clear")
+        print("ข้อมูลไม่ถูกต้อง่!")
+        time.sleep(2)
+    
+    nameWorker = input("ชื่อคนงาน: ")
+    if nameWorker == "":
+        nameWorker = "poppy"
+    
+    push = {
+        'Pool': pool,
+        'Wallet': wallet,
+        'Pass': f"c={coin.upper()},mc=VRSC,ID={nameWorker}",
+        'Cpu': cpu_thread-1
+    }
+    with open("set-miner/miner.json", "w") as set:
+        json.dump(push, set, indent=4)
+
+
+
+def presetTwo(): # luckpool
+    os.system("@cls||clear")
+    print("[--Time Zone--]\n"
+    ," [1] North America\n"
+    ," [2] Europe\n"
+    ," [3] Asia-Pacific")
+    try:
+        option = int(input(">>> "))
+        if option > 3 or option < 1:
+            raise Exception()
+        elif option == 1:
+            pool = "stratum+tcp://na.luckpool.net:3956"
+        elif option == 2:
+            pool = "stratum+tcp://eu.luckpool.net:3956"
+        elif option == 3:
+            pool = "stratum+tcp://ap.luckpool.net:3956"
+    except:
+        os.system("@cls||clear")
+        print("เลือก 1 - 4 เท่านั้น!")
+        time.sleep(2)
+
+    coin = input("pass[-p]: ")
+    if coin == "":
+        coin = "x"
+
+    wallet = input("ที่อยู่กระเป๋า VRSC : ")
+    if wallet == "":
+        os.system("@cls||clear")
+        print("ข้อมูลไม่ถูกต้อง่!")
+        time.sleep(2)
+    
+    nameWorker = input("ชื่อคนงาน: ")
+    if nameWorker == "":
+        nameWorker = "poppy"
+
+    push = {
+        'Pool': pool,
+        'Wallet': wallet+"."+nameWorker,
+        'Pass': coin,
+        'Cpu': cpu_thread-1
+    }
+    with open("set-miner/miner.json", "w") as set:
+        json.dump(push, set, indent=4)
+
+
+def saveMiner():
+    pass
+
+def editMiner():
+    pass
+
 
 # setting function
-def set_miner():
-    banner(edit_banner)
-    pool = None
-    wallet = None
-    password = None
-    cpu = None
+def custom():
+    banner()
     try:
         print("ตัวอย่าง: \033[93mstratum+tcp://ap.luckpool.net:3956\033[00m")
-        pool = input("Pool[-o]: ")
+        pool = input("[-o]: ")
 
         print("ตัวอย่าง: \033[93mRQpWNdNZ4LQ5yHUM3VAVuhUmMMiMuGLUhT.OMG-MINER\033[00m")
-        wallet = input("Wallet[-u]: ")
+        wallet = input("[-u]: ")
 
         print("ตัวอย่าง: \033[93mx หรือ ( hybrid เฉพาะ luckpool )\033[00m")
-        password = input("Password[-p]: ")
+        password = input("[-p]: ")
 
-        print("\033[93m 0 ขึ้นไป หรือ เท่ากับจำนวณเธรดCPUท่าน เช็ค 'lscpu'\033[00m")
-        cpu = int(input("CPU[-t]: "))
+        print(f"\033[93mตัวอย่าง: 0 > {cpu_thread}\033[00m")
+        cpu = int(input("[-t]: "))
         
         if pool == "" or wallet == "":
             raise Exception()
@@ -50,24 +131,62 @@ def set_miner():
     except:
         print("\nเกิดข้อผิดพลาด มีบางอย่างไม่ถุูกต้อง!")
         time.sleep(2)
-        set_miner()
-    puts = {
+        custom()
+    push = {
         'Pool': pool,
         'Wallet': wallet,
         'Pass': password,
         'Cpu': cpu
     }
     with open("set-miner/miner.json", "w") as set:
-        json.dump(puts, set, indent=4)
+        json.dump(push, set, indent=4)
 
-# check path & main process
-os.system("clear")
-with MoonSpinner("กำลังทำงาน...") as bar:
-        for i in range(100):
-            time.sleep(0.05)
-            bar.next()
-if os.path.exists("set-miner") == True:
-    set_miner()
-else:
-    os.system("mkdir set-miner")
-    
+
+
+
+def router(option):
+    if option == 1:
+        presetOne()
+    elif option == 2:
+        presetTwo()
+    elif option == 3:
+        saveMiner()
+    elif option == 4:
+        editMiner()
+    elif option == 5:
+        custom()
+
+
+
+
+while True:
+    # check path & main process
+    os.system("@cls||clear")
+    with MoonSpinner("กำลังทำงาน...") as bar:
+            for i in range(100):
+                time.sleep(0.05)
+                bar.next()
+    if os.path.exists("set-miner") == True:
+        banner()
+        print(f"[--เมนู--]  \033[0;37;44mCPU = {cpu_thread}\033[0;37;40m\n"
+        ," [1] Preset Zergpool\n"
+        ," [2] Preset Luckpool\n"
+        ," [3] ที่บันทึกไว้\n"
+        ," [4] แก้ไขการตั้งค่าปัจจุบัน\n"
+        ," [5] กำหนดเอง\n"
+        ," [0] ออก")
+        try:
+            option = int(input(">>> "))
+            if option > 5:
+                raise Exception()
+            elif option == 0:
+                break
+            router(option)
+        except:
+            os.system("@cls||clear")
+            print("เลือก 1 - 5 และ 0 เท่านั้น!")
+            time.sleep(2)
+
+    else:
+        os.system("mkdir set-miner")
+
--- src_run_ccminer/main.py
+++ src_run_ccminer/main.py
@@ -1,43 +1,34 @@
-import os, json, time
-from progress.spinner import MoonSpinner
+import os, json, time, pip
+from config import banner
 
-# banner
-setting_banner = """
-███████╗███████╗████████╗████████╗██╗███╗   ██╗ ██████╗ 
-██╔════╝██╔════╝╚══██╔══╝╚══██╔══╝██║████╗  ██║██╔════╝ 
-███████╗█████╗     ██║      ██║   ██║██╔██╗ ██║██║  ███╗
-╚════██║██╔══╝     ██║      ██║   ██║██║╚██╗██║██║   ██║
-███████║███████╗   ██║      ██║   ██║██║ ╚████║╚██████╔╝
-╚══════╝╚══════╝   ╚═╝      ╚═╝   ╚═╝╚═╝  ╚═══╝ ╚═════╝"""
 
-running_banner = """
-██████╗ ██╗   ██╗███╗   ██╗         
-██╔══██╗██║   ██║████╗  ██║         
-██████╔╝██║   ██║██╔██╗ ██║         
-██╔══██╗██║   ██║██║╚██╗██║         
-██║  ██║╚██████╔╝██║ ╚████║██╗██╗██╗
-╚═╝  ╚═╝ ╚═════╝ ╚═╝  ╚═══╝╚═╝╚═╝╚═╝"""
 
-# banner function
-def banner(logo):
-    os.system("clear")
-    print(logo,"\nCreated by.mobile-mining")
-    print("---------------------------------------------------") 
-    print("\033[96mสนับสนุนนักพัมนา\033[00m\n"
-        + " กสิกรไทย: 0608905863\n"
-        + "     LTC:  LTT2X57ervCMpfNva5uo1JBzT2UJVBiEZu\n"
-        + "    DOGE:  DFwUqKS3j3RMMkqQF8z6kbYZ652M8VCFVs")
-    print("---------------------------------------------------\n")
+# check import module
+try:
+    from progress.spinner import MoonSpinner
+except ImportError:
+    pip.main(['install', '--user', 'progress'])
+    from progress.spinner import MoonSpinner
+
+
+
+
 
 
 # install miner function 
 def install():
-    # os.system("git clone --single-branch -b ARM https://github.com/monkins1010/ccminer")
-    os.system("git clone https://github.com/mantvmass/ccminer_mmv")
+    try:
+        # os.system("git clone --single-branch -b ARM https://github.com/monkins1010/ccminer")
+        os.system("git clone https://github.com/mantvmass/ccminer_mmv")
+        os.system("@cls||clear")
+        print("ติดตั้งสำเร็จ")
+    except:
+        print("ติดตั้งไม่สำเร็จ!")
+
 
 # run miner function
 def run():
-    banner(running_banner)
+    banner()
     with open("set-miner/miner.json", encoding="utf-8") as set:
         load = set.read()
         loads = json.loads(load)
@@ -46,55 +37,17 @@ def run():
         password = loads['Pass']
         cpu = loads['Cpu']
     if pool == "" or wallet == "":
-        print("pool หรือ wallet กรุณาตั้งค่าใหม่")
-        time.sleep(3)
-        set_miner()
-    # print(pool, wallet, password, cpu)
+        print("ไม่พบการตั้งค่า miner กรุณาตั้งค่าโดยใช้คำสั่ง edit-miner")
+        return
     print("ccminer CPU3.7 for VerusHash v2.1 - 2.2 by Monkins1010 based on ccminer")
     print("Originally based on Christian Buchner and Christian H. project")
-    print("\033[93mLocated at\033[00m: http://github.com/monkins1010/ccminer")
     os.system(f"cd ccminer_mmv && ./ccminer -a verus -o {pool} -u {wallet} -p {password} -t {cpu}")
 
-def set_miner():
-    banner(setting_banner)
-    pool = None
-    wallet = None
-    password = None
-    cpu = None
-    try:
-        print("ตัวอย่าง: \033[93mstratum+tcp://ap.luckpool.net:3956\033[00m")
-        pool = input("Pool[-o]: ")
 
-        print("ตัวอย่าง: \033[93mRQpWNdNZ4LQ5yHUM3VAVuhUmMMiMuGLUhT.OMG-MINER\033[00m")
-        wallet = input("Wallet[-u]: ")
 
-        print("ตัวอย่าง: \033[93mx หรือ ( hybrid เฉพาะ luckpool )\033[00m")
-        password = input("Password[-p]: ")
-
-        print("\033[93m 0 ขึ้นไป หรือ เท่ากับจำนวณเธรดCPUท่าน เช็ค 'lscpu'\033[00m")
-        cpu = int(input("CPU[-t]: "))
-
-        if pool == "" or wallet == "":
-            raise Exception()
-        if password == "":
-            password = "x"
-        if cpu == "":
-            cpu = 1
-    except:
-        print("\nเกิดข้อผิดพลาด มีบางอย่างไม่ถุูกต้อง!")
-        time.sleep(2)
-        set_miner()
-    puts = {
-        'Pool': pool,
-        'Wallet': wallet,
-        'Pass': password,
-        'Cpu': cpu
-    }
-    with open("set-miner/miner.json", "w") as set:
-        json.dump(puts, set, indent=4)
 
 while True:
-    os.system("clear")
+    os.system("@cls||clear")
     with MoonSpinner("กำลังทำงาน...") as bar:
         for i in range(100):
             time.sleep(0.05)
@@ -107,9 +60,8 @@ while True:
             run()
             break
         else:
-            set_miner()
+            os.system("@cls||clear")
+            print("ไม่พบการตั้งค่า miner กรุณาตั้งค่าโดยใช้คำสั่ง edit-miner")
     else:
-        os.system("mkdir set-miner")
-
-    
-    
+        os.system("@cls||clear")
+        print("ไม่พบการตั้งค่า miner กรุณาตั้งค่าโดยใช้คำสั่ง edit-miner")
